a298ef22ec5702a0f29bd1d75ca7d646fbf52ea3
Carlos Daniel <carlosdaniellaportecastro@gmail.com>
2026-03-11 17:12:43 -0300

    coloquei alguns popup na tela de login cadastro e dashboard, e modificando a user_add

--- product/pages/login.php
+++ product/pages/login.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     $error_message = '';
+    $success_message = '';
 
     require_once('../configuration/connection.php');
     require_once('../authentication/guest.php');
@@ -37,70 +38,64 @@
 
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IMS Login - Inventory Management System</title>
-    <link rel="stylesheet" href="../CSS/styleLogin.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-</head>
-<body id="loginBody">
-    <?php
-            if(!empty($error_message)){ ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>IMS Login - Inventory Management System</title>
+        <link rel="stylesheet" href="../CSS/styleLogin.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+    <body id="loginBody">
+        <div class="container">
+            <div class="loginHeader">
+                <h1>IMS</h1>
+                <p>INVENTORY MANAGEMENT SYSTEM</p>
+            </div>
+            <div class="loginBody">
+                <form action="login.php" method="POST">
+                    <div class="topIcon">
+                        <span><i class="bi bi-lock-fill"></i></span>
+                    </div>
+                    <div class="topText">
+                        <h2>Welcome Back!</h2>
+                        <p>Login to your account to manage your inventory.</p>
+                    </div>
+                    <div class="loginInputsConteiner">
+                        <label for="email">Email</label>
+                        <input type="email" placeholder="Email" name="email">
+                    </div>
+                    <div class="loginInputsConteiner">
+                        <label for="password">Password</label>
+                        <input type="password" placeholder="Password" name="password">
+                    </div>
+                    <div class="loginButtonConteiner">
+                        <button>Login</button>
+                    </div>
+                    <div class="textIcons">
+                        <div class="line"></div>
+                        <div class="text">Or login with</div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="iconsLogin">
+                        <a href="../oauth/google-login.php">
+                            <i class="bi bi-google"></i>
+                        </a>
 
-                <div id="errorMessage">
-                    <p>Error: <?= $error_message ?></p>
-                </div>
+                        <a href="../oauth/github-login.php">
+                            <i class="bi bi-github"></i>
+                        </a>
 
-    <?php   } ?>
-    <div class="container">
-        <div class="loginHeader">
-            <h1>IMS</h1>
-            <p>INVENTORY MANAGEMENT SYSTEM</p>
+                        <a href="../oauth/facebook-login.php">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                    </div>
+                    <div class="loginCadastrar">
+                        <p>Don't have an account yet?<a href="cadastro.php"><strong>Sign up!</strong></a></p>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="loginBody">
-            <form action="login.php" method="POST">
-                <div class="topIcon">
-                    <span><i class="bi bi-lock-fill"></i></span>
-                </div>
-                <div class="topText">
-                    <h2>Welcome Back!</h2>
-                    <p>Login to your account to manage your inventory.</p>
-                </div>
-                <div class="loginInputsConteiner">
-                    <label for="email">Email</label>
-                    <input type="email" placeholder="Email" name="email">
-                </div>
-                <div class="loginInputsConteiner">
-                    <label for="password">Password</label>
-                    <input type="password" placeholder="Password" name="password">
-                </div>
-                <div class="loginButtonConteiner">
-                    <button>Login</button>
-                </div>
-                <div class="textIcons">
-                    <div class="line"></div>
-                    <div class="text">Or login with</div>
-                    <div class="line"></div>
-                </div>
-                <div class="iconsLogin">
-                    <a href="../oauth/google-login.php">
-                        <i class="bi bi-google"></i>
-                    </a>
-
-                    <a href="../oauth/github-login.php">
-                        <i class="bi bi-github"></i>
-                    </a>
-
-                    <a href="../oauth/facebook-login.php">
-                        <i class="bi bi-facebook"></i>
-                    </a>
-                </div>
-                <div class="loginCadastrar">
-                    <p>Don't have an account yet?<a href="cadastro.php"><strong>Sign up!</strong></a></p>
-                </div>
-            </form>
-        </div>
-    </div>
-</body>
+        <?php require_once('../includes/popup.php'); ?>
+    </body>
 </html>
--- product/pages/user_add.php
+++ product/pages/user_add.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     $error_message = '';
+    $sucess_message = '';
 
     require_once('../configuration/connection.php');
 
@@ -32,7 +33,6 @@
         $lastName        = trim($_POST['last_name']);
         $email           = strtolower(trim($_POST['email']));
         $password        = trim($_POST['password']);
-        $passwordConfirm = trim($_POST['password_confirm']);
 
         if (empty($firstName) || empty($lastName) || empty($email) || empty($password)) {
             $error_message = "Please fill in all fields.";
@@ -57,7 +57,10 @@
                         ':email'      => $email,
                         ':password'   => $hashedPassword
                     ]);
-                    
+
+                    if($result) {
+                        $sucess_message = "User added successfully.";
+                    }
     
                 } catch (PDOException $e) {
                     $error_message = "Database Error: " . $e->getMessage();
@@ -75,45 +78,46 @@
         <title>Dashboard - Inventory Management System</title>
         <link rel="stylesheet" href="../CSS/styleDashboard.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body>
         <div id="dashboardMainConteiner">
             <?php require_once('../includes/sidebar.php'); ?>
             <div class="dashboard_content_conteiner" id="dashboard_content_conteiner">
-                <?php
-                    if(!empty($error_message)) { ?>
-
-                        <div id="errorMessage">
-                            <p>Error: <?= htmlspecialchars($error_message) ?></p>
-                        </div>
-
-                <?php   } ?>
                 <?php require_once('../includes/nav.php') ?>
                 <div class="dashboard_espaço">
                 </div>
                 <div class="dashboard_content">
                     <div id="dashboard_content_form">
-                        <form method="POST"  class="userForm">
-                            <div class="registerInputsConteiner">
-                                <label for="first_name">First name</label>
-                                <input type="text" placeholder="First name" name="first_name" required>
+                        <div class="row">
+                            <div class="column column-4">
+                                <h1 class="textTopForm"><i class="bi bi-plus"></i> Create User</h1>
+                                <form method="POST"  class="userForm">
+                                    <div class="registerInputsConteiner">
+                                        <label for="first_name">First name</label>
+                                        <input type="text" placeholder="First name" name="first_name" required>
+                                    </div>
+                                    <div class="registerInputsConteiner">
+                                        <label for="last_name">Last name</label>
+                                        <input type="text" placeholder="Last name" name="last_name" required>
+                                    </div>
+                                    <div class="registerInputsConteiner">
+                                        <label for="email">Email</label>
+                                        <input type="email" placeholder="Email" name="email" required>
+                                    </div>
+                                    <div class="registerInputsConteiner">
+                                        <label for="password">Password</label>
+                                        <input type="password" placeholder="Password" name="password" required>
+                                    </div>
+                                    <div class="registerButtonConteiner">
+                                        <button><i class="bi bi-plus-lg"></i>Add User</button>
+                                    </div>
+                                </form>   
                             </div>
-                            <div class="registerInputsConteiner">
-                                <label for="last_name">Last name</label>
-                                <input type="text" placeholder="Last name" name="last_name" required>
+                            <div class="column column-6">
+                                <p>hi</p>
                             </div>
-                            <div class="registerInputsConteiner">
-                                <label for="email">Email</label>
-                                <input type="email" placeholder="Email" name="email" required>
-                            </div>
-                            <div class="registerInputsConteiner">
-                                <label for="password">Password</label>
-                                <input type="password" placeholder="Password" name="password" required>
-                            </div>
-                            <div class="registerButtonConteiner">
-                                <button><i class="bi bi-plus-lg"></i>Add User</button>
-                            </div>
-                        </form>   
+                        </div>
                     </div>
                 </div>
                 <div class="dashboard_content_footer">
@@ -122,5 +126,6 @@
             </div>
         </div>
         <script src="../assets/scripts.js"></script>
+        <?php require_once('../includes/alerts.php'); ?>
     </body>
 </html>
